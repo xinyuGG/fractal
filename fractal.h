c0d5d4a3a139bd258c4ce7b38c00ae435f772d60
xinyuGG <774794770@qqq.com>
2018-11-16 20:28:35 +0800

    async并行化

---
+++ fractal.h
@@ -0,0 +1,58 @@
+﻿#include <stdint.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+#include <complex>
+
+
+//extern const double RMIN, RMAX , IMIN , IMAX;
+//extern const int W ;
+//const double RESN = W / (RMAX - RMIN);
+//const int H = (IMAX - IMIN) * RESN;
+//extern const int MI;
+class Bmp256 {
+#pragma pack(2)
+	struct Header {
+		uint16_t bfType = 0x4D42;
+		uint32_t bfSize;
+		uint16_t bfReserved1 = 0;
+		uint16_t bfReserved2 = 0;
+		uint32_t bfOffBits = 54 + 256 * 4;
+		uint32_t biSize = 40;
+		int32_t  biWidth;
+		int32_t  biHeight;
+		uint16_t biPlanes = 1;
+		uint16_t biBitCount = 8;
+		uint32_t biCompression = 0;
+		uint32_t biSizeImage = 0;
+		int32_t  biXPelsPerMeter = 0;
+		int32_t  biYPelsPerMeter = 0;
+		uint32_t biClrUsed = 256;
+		uint32_t biClrImportant = 256;
+	} header;
+#pragma pack()
+	int32_t rowSize;
+	struct {
+		uint8_t B, G, R, A;
+		void set(uint8_t r, uint8_t g, uint8_t b) { R = r; G = g; B = b; A = 0; }
+	} palette[256];
+	uint8_t *buffer = nullptr;
+
+	void calc_palette();
+
+public:
+	Bmp256(int width, int height);
+	~Bmp256() { delete[] buffer; }
+	int width()  const { return header.biWidth; }
+	int height() const { return header.biHeight; }
+	uint8_t& operator()(int row, int col) { return buffer[row * rowSize + col]; }	// get/set the pixel
+	void save(const char* file);
+	uint8_t* begin();
+
+	uint8_t* end();
+
+};
+
+
+
---
+++ parallel.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+#include <complex>
+#include <thread>
+#include <future>
+#include <mutex>
+using complex = std::complex<double>;
+
+const double RMIN = -2, RMAX = 1, IMIN = -1, IMAX = 1;
+const int W = 12*1024;
+const double RESN = W / (RMAX - RMIN);
+const int H = (IMAX - IMIN) * RESN;
+const int MI = 1;
+const int SLICE = 32;
+class Parallel {
+public:
+	static int Mandelbrot(complex&& c) {
+		complex z;
+		for (int k = 256 * MI - 1; k >= 0; --k) {
+			z = z * z + c;
+			if (std::norm(z) > 4) return k / MI;
+		}
+		return 0;
+	}
+	template<typename Iterator>
+	static int Mandelbrot(Iterator first,const int W, const int index, const int h_num) {
+		Iterator it = first;
+		for (int h = 0; h < h_num; h++) {
+			auto l_index = index + h;
+			for (int i = 0; i < W; it++, i++) {
+				*it = Mandelbrot(complex{ RMIN + i / RESN, IMIN + l_index / RESN });
+			}
+		}
+
+		return 0;
+	}
+	template<typename Iterator> 
+	static int Mandelbrot_pall(Iterator first, Iterator last, const int W, int H, const int index) {
+		const unsigned long length = std::distance(first, last) ;
+		if (!length)
+			return 0;
+		if (length <= W * SLICE)
+		{
+			Mandelbrot(first, W, index, SLICE);
+			return 0;
+		}
+		else {
+			int half_length = H / 2;
+			Iterator mid = first + half_length * W;
+			std::future<int> first_half = std::async(&Mandelbrot_pall<Iterator>, first, mid, W, half_length, index);
+			Mandelbrot_pall<Iterator>(mid, last, W, H - half_length, index + half_length);
+
+		}
+	}
+
+};
