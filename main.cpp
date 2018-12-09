ff7fa3668c307b7695fbe81fdaf933a7e6590f93
xinyuGG <774794770@qq.com>
2018-12-10 04:54:14 +0800

    最终版本提交

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "parallel.h"
 using namespace std::chrono;
 using namespace Feng;
+
 int main() {
 	Bmp256 bmp{ W, H };
 	int num = 5;
@@ -19,10 +20,10 @@ int main() {
 	t1 = steady_clock::now();
 	for (int i = 0; i < num; i++)
 		Parallel::Mandelbrot_pall<uint8_t*>(bmp.begin(), bmp.end(), W, H, 0);
-	t2 = steady_clock::now();
+		t2 = steady_clock::now();
 	std::cout << "para run time: " << duration_cast<duration<double>>(t2 - t1).count() / (double)num << " seconds.\n";
 
-	// bmp.save("Mandelbrot12k.bmp");
+	bmp.save("Mandelbrot12k.bmp");
 
 
 // #ifdef _MSC_VER
